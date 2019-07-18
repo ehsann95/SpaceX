c22f207aad6dfab9001655a8fbb417a28eb2c4f6
Ahsan <35002844+ehsann95@users.noreply.github.com>
2019-07-18 12:16:49 +0530

    Add files via upload

---
+++ public/precache-manifest.aa5fc44955a1308b287a393bb5c3df8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da840f9d29c108aabda9a8eabb5a470",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4354679ed41e5e2b579",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "39a1a5bbdf0ae07f73fd",
+    "url": "/static/js/2.ea9101f6.chunk.js"
+  },
+  {
+    "revision": "a4354679ed41e5e2b579",
+    "url": "/static/js/main.d8e0621e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "be955c3307db00605eee559b22e0b02e",
+    "url": "/static/media/logo.be955c33.png"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
+const schema = require("./schema.js");
+const path = require("path");
+
+const app = express();
+
+// Allow cross-origin. can make requests from one domain to another(localhost 3000 to 5000)
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
